2729debb15883ee817703be8a5f77a002b4b181c
Josh Ault <jault@sofi.org>
2018-10-05 12:03:51 -0600

    fixed it

---
+++ src/main/java/com/amazon/ask/helloworld/handlers/HelloWorldStreamHandler.java
@@ -0,0 +1,24 @@
+package com.amazon.ask.helloworld.handlers;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+
+public class HelloWorldStreamHandler extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(
+                        new CancelAndStopIntentHandler(),
+                        new HelloWorldIntentHandler(),
+                        new HelpIntentHandler(),
+                        new LaunchRequestHandler(),
+                        new SessionEndedRequestHandler())
+                .build();
+    }
+
+    public HelloWorldStreamHandler() {
+        super(getSkill());
+    }
+
+}
