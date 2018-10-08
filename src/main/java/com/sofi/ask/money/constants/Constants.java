3dad8739b391520ae25a03dbf35549cac06d4450
Marcus Yarbro <myarbro@sofi.org>
2018-10-08 09:08:55 -0600

    fixed tonal on SoFi

--- src/main/java/com/sofi/ask/money/constants/Constants.java
+++ src/main/java/com/sofi/ask/money/constants/Constants.java
@@ -1,5 +1,5 @@
 package com.sofi.ask.money.constants;
 
 public class Constants {
-    public static final String SOFI_NAME = "<phoneme alphabet=\'ipa\' ph=\'soʊfaɪ\'>SoFi</phoneme>";
+    public static final String SOFI_NAME = "<phoneme alphabet=\'ipa\' ph=\'ˌsofaɪ\'>SoFi</phoneme>";
 }
